d5ac21d9da5629103576bba4669d57e3ef7121e7
mwcsisk <mwcsisk@gmail.com>
2020-07-12 17:11:53 -0700

    Added basic entry rendering

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,8 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "entry": util.get_entry(title)
+    })
